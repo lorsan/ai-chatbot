cfda6ceb0335c40c8d742df3e8da1cac8af21424
Alessandro Ercolani <ale.ercolani@gmail.com>
2024-02-24 16:14:58 +0100

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -47,7 +47,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
           <div className="flex min-h-screen flex-col">
             {/* @ts-ignore */}
             <Header />
-            <main className="flex flex-1 flex-col bg-muted/50 bg-white">{children}</main>
+            <main className="flex flex-1 flex-col bg-muted/50">{children}</main>
           </div>
           {/* <TailwindIndicator /> */}
         </Providers>
--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -15,7 +15,7 @@ export default async function SignInPage() {
 <div className="flex min-h-screen bg-[url('/lbackground.jpg')] justify-center items-center">
   <div className="session flex flex-col sm:flex-row bg-slate-400 rounded-lg shadow-md">
     {/* Imposta l'immagine come cover sia in modalità mobile che desktop */}
-    <div className="left w-full sm:w-64 h-64 sm:h-auto bg-[url('/indianbot.png')] bg-no-repeat bg-center bg-cover rounded-t-lg sm:rounded-l-lg sm:rounded-tr-none"></div>
+    <div className="left w-full sm:w-64 h-64 sm:h-auto bg-[url('/indianbot.png')] bg-no-repeat bg-cover rounded-t-lg sm:rounded-l-lg sm:rounded-tr-none"></div>
 
     <div className="log-in flex flex-col items-center sm:items-start bg-white p-8 sm:p-16 md:p-32 w-full">
       <h4 className="font-semibold text-xl sm:text-2xl mb-2">
