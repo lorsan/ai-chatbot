ea474134cd80774d32b8b3e6eca623eb60e497b4
Lorenzo <16209989+lorsan@users.noreply.github.com>
2024-03-04 18:10:43 +0100

    cambiata la landing di sibilla

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -12,17 +12,18 @@ export default async function SignInPage() {
   }
   return (
     //  Big Header, title, subtitile and login button.
-<div className="flex min-h-screen bg-[url('/lbackground.jpg')] justify-center items-center">
-  <div className="session flex flex-col sm:flex-row bg-slate-400 rounded-lg shadow-md">
+<div className="flex min-h-screen bg-white justify-center items-center">
+  <div className="session flex flex-col sm:flex-row rounded-lg shadow-md m-8">
     {/* Imposta l'immagine come cover sia in modalità mobile che desktop */}
-    <div className="left w-full sm:w-64 h-64 sm:h-auto bg-[url('/indianbot.png')] bg-no-repeat bg-cover rounded-t-lg sm:rounded-l-lg sm:rounded-tr-none"></div>
+    <div className="left w-full h-64 bg-center bg-cover rounded-t-lg sm:w-64 sm:h-auto sm:rounded-l-lg sm:rounded-tr-none" style={{ backgroundImage: "url('/indianbot.png')" }}></div>
 
     <div className="log-in flex flex-col items-center sm:items-start bg-white p-8 sm:p-16 md:p-32 w-full">
       <h4 className="font-semibold text-xl sm:text-2xl mb-2">
         Ciao sono <span className="font-bold">SIBILLA 🤠</span>
       </h4>
-      <p className="text-base sm:text-lg text-gray-600 mb-4 text-center sm:text-left">
-        Ti guiderò alla scoperta della storia della Val di Non.<br />Accedi e chiedimi quello che vuoi.
+      <p className="text-base sm:text-lg text-gray-600 mb-4 sm:text-left">
+        
+      I'm Sibilla, your AI-powered virtual guide fluent in 25 languages, here to enrich your exploration of museums, exhibitions, and historical sites with detailed information and captivating stories. I tailor recommendations to your interests, transforming cultural outings into unforgettable experiences. Register now and embark on a global cultural journey with me.
       </p>
       <LoginButton className="bg-purple-200 text-purple-700 font-semibold py-2 sm:py-3 px-4 rounded-lg shadow-md hover:bg-purple-300 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-opacity-50 text-md sm:text-lg" />
     </div>
