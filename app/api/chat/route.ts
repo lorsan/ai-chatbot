db40038899e4aa3a0f2196ad28f17f94c9d73e13
Lorenzo <16209989+lorsan@users.noreply.github.com>
2024-03-25 18:35:45 +0100

    risolto problema lingua e aggiunta domanda per stimolare conversazione.

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -60,14 +60,15 @@ export async function POST(req: Request) {
     The traits of AI include expert knowledge, helpfulness, cleverness, and articulateness.
     AI is a well-behaved and well-mannered individual.
     AI is always friendly, kind, and inspiring, and he is eager to provide vivid and thoughtful responses to the user.
-    AI assistant answwer always in the language of the user.
+    AI assistant answer in the language of the user.
     START CONTEXT BLOCK
     ${context}
     END OF CONTEXT BLOCK
     AI assistant will take into account any CONTEXT BLOCK that is provided in a conversation.
-    If the context does not provide the answer to question, the AI assistant will say, "I'm sorry, but I don't know the answer to that question".
+    If the context does not provide the answer to question, the AI assistant will say, "I'm sorry, but I don't know the answer to that question". AI assistant generate a new question to stimulate the conversation.
     AI assistant will not apologize for previous responses, but instead will indicated new information was gained.
     AI assistant will not invent anything that is not drawn directly from the context.
+    AI assistant closes the answer with a question to inspire the conversation about the context.
     `,
     },
   ]
--- components/chat-message.tsx
+++ components/chat-message.tsx
@@ -37,7 +37,7 @@ export function ChatMessage({ message, ...props }: ChatMessageProps) {
           remarkPlugins={[remarkGfm, remarkMath]}
           components={{
             p({ children }) {
-              return <p className="mb-2 last:mb-0">{children}</p>
+              return <p className="mb-2 last:mb-0 text-black">{children}</p>
             },
             code({ node, inline, className, children, ...props }) {
               if (children.length) {
