5425fdb4896c6cc04fb4d29fe8ba756fcfb1cbc0
Lorenzo <16209989+lorsan@users.noreply.github.com>
2024-08-30 13:18:02 +0200

    aggiornati bottoni

--- components/header.tsx
+++ components/header.tsx
@@ -34,7 +34,7 @@ export async function Header() {
             </h1>
 
             <p className="mt-1.5 text-sm text-gray-500">
-              Scopri la storia del tuo paese.
+              Scopri la storia con noi.
             </p>
           </div>
 
@@ -43,10 +43,7 @@ export async function Header() {
               className="inline-flex items-center justify-center gap-1.5 rounded border border-gray-200 bg-white px-5 py-3 text-gray-900 transition hover:text-gray-700 focus:outline-none focus:ring"
               type="button"
             >
-              <span className="text-sm font-medium">
-                {' '}
-                Visita il sito del Comune{' '}
-              </span>
+              <span className="text-sm font-medium"> Torna alla Home </span>
 
               <svg
                 xmlns="http://www.w3.org/2000/svg"
